8afb5ebac846db3e2a405f8eabf9c5587a558d4f
Theodor Skippervold <theodor.skippervold@vipps.no>
2018-10-04 19:59:17 +0200

    Initial Commit

---
+++ Swiper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiper
+//
+//  Created by Theodor Tomander Skippervold on 04/10/2018.
+//  Copyright © 2018 Theodor Tomander Skippervold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
